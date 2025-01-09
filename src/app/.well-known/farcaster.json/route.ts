26921594fd28131f78fb7ea871537d760a7f1c26
cashlessman <cashlessman@proton.me>
2025-01-09 23:07:55 +0530

    5

--- src/app/.well-known/farcaster.json/route.ts
+++ src/app/.well-known/farcaster.json/route.ts
@@ -1,13 +1,12 @@
 export async function GET() {
   const appUrl = process.env.NEXT_PUBLIC_URL;
 
+  
   const config = {
     accountAssociation: {
-      header:
-        "eyJmaWQiOjM2MjEsInR5cGUiOiJjdXN0b2R5Iiwia2V5IjoiMHgyY2Q4NWEwOTMyNjFmNTkyNzA4MDRBNkVBNjk3Q2VBNENlQkVjYWZFIn0",
-      payload: "eyJkb21haW4iOiJmcmFtZXMtdjIudmVyY2VsLmFwcCJ9",
-      signature:
-        "MHhiNDIwMzQ1MGZkNzgzYTExZjRiOTllZTFlYjA3NmMwOTdjM2JkOTY1NGM2ODZjYjkyZTAyMzk2Y2Q0YjU2MWY1MjY5NjI5ZGQ5NTliYjU0YzEwOGI4OGVmNjdjMTVlZTdjZDc2YTRiMGU5NzkzNzA3YzkxYzFkOWFjNTg0YmQzNjFi",
+      header: "eyJmaWQiOjI2ODQzOCwidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDIxODA4RUUzMjBlREY2NGMwMTlBNmJiMEY3RTRiRkIzZDYyRjA2RWMifQ",
+      payload: "eyJkb21haW4iOiJkZWdlbi1vZy52ZXJjZWwuYXBwIn0",
+      signature: "MHg5ZjgzYTllOWU5ODQxN2FhODVlYmNiNzAwMzE4MjY1Y2ZjYjQwNDk2MzRjZGEyYWJlNmZiYjA2YTMxYWFlYjJjMmNlYjkwZjkxZDQxNWFmZjE4ZDVhYTlmZmJjMmE5ZmY5YWRmMDE4NDA5NDRmN2I0NzkyN2JhNzMyOWY2YjIwMDFj"
     },
     frame: {
       version: "1",
